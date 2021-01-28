bed293a33c82616943b17a004ae153552b8f42ae
Rayan Khan <iRayanKhan@iCloud.com>
2021-01-27 22:48:05 -0600

    Add files via upload
    Updated file.

--- mathAndVariables.py
+++ mathAndVariables.py
@@ -1,17 +1,47 @@
+import math;
+a = 5.0
+b = 3
+c = 100
+d = 4
 x = 2
 y = 5
 z = 7
 
-# Add 
-print (x + y)
+# Addition: 
 
-# Subtract
-print (z - x)
+print (x + y) # (2 + 5) = 7
 
-# Multiply
-print (x * y)
+# Subtract: 
 
-# Divide
+print (z - x) # (7 - 2) = 5
 
-print (y / x)
+# Multiply: 
+
+print (x * y) # (2 * 5) = 10
+
+# Divide: 
+
+print (y / x) # (5 / 2) = 2
+
+# Floor Divison: 
+
+print (a / x)  # (5.0 / 2)  = 2.5
+print (a // x) # (5.0 // 2) = 2.0
+
+# Modulus:
+
+print (c % x) # (100 % 2) = 0
+
+# Powers: (using artighmetic operator symbols)
+
+print (pow(b,d)) # (pow(3,4)) = 81
+
+# Roots: 
+print (math.sqrt(a)) # (sqrt of 5.0) = 2.2360679775
+
+# Paranthesis:
+
+print (((a*d)) // x) # (5.0 * 4) / 4 = 20/2 = 10
+
+
 
